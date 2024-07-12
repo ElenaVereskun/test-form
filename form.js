be8f382b9322136c08b91ab45cf2bf18accd21b4
ElenaVereskun <lena.vereskun@yandex.ru>
2024-07-12 20:06:10 +0300

    form

--- form.js
+++ form.js
@@ -1,17 +1,34 @@
-$(document).ready(function () {
-  $("btn-primary").click(function () {
-    sendAjaxForm("result_form", "ajax_form", "action_ajax_form.json");
-    return false;
-  });
-});
+const form = document.getElementById("myForm");
 
-function sendAjaxForm(result_form, ajax_form, url) {
-  $.ajax({
-    dataType: "json",
-    url: url,
-    data: $(ajax_form).serialize(),
-    success: function (data) {
-      $(result_form).html(data);
-    },
-  });
+function formDataToObject(formData) {
+  const dataValues = (values) => (values.length > 1 ? values : values[0]);
+  const formElemKeys = Array.from(formData.keys());
+
+  return Object.fromEntries(
+    formElemKeys.map((key) => [key, dataValues(formData.getAll(key))])
+  );
 }
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const formData = new FormData(form);
+  const jsonObject = formDataToObject(formData);
+  const jsonString = JSON.stringify(jsonObject);
+
+  $(".form")
+    .parent()
+    .append($("<div>", { text: jsonString }));
+
+  const xhr = new XMLHttpRequest();
+  xhr.method = "GET";
+  xhr.onload = function (event) {
+    if (this.status == 200) {
+      alert(jsonString);
+    } else {
+      alert("Ошибка: " + this.status);
+    }
+  };
+  xhr.open("GET", "my_data.json");
+  xhr.send();
+});
